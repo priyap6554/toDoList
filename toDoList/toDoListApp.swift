261ca99b5ba132b887b7ed9d14507fd500ec78e0
Priya <priyapatel7989@gmail.com>
2023-08-17 17:23:11 -0500

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by scholar on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
